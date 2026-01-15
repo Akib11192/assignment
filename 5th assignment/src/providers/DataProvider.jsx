86be43c46938a9ecd5e20d50cad813d74193cc8f
akib <akibgoto@gmail.com>
2026-01-15 14:17:48 +0600

    try

--- 5th assignment/src/providers/DataProvider.jsx
+++ 5th assignment/src/providers/DataProvider.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { DataContext } from "../contexts/dataContext";
+import { DataContext } from "../contexts/dataContext.js";
 
 const DataProvider = ({ children }) => {
   const [contacts, setContacts] = useState([]);
