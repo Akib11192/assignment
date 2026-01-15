f341d2fd7a3fcff57fe5006236e75b552ac9ff39
akib <akibgoto@gmail.com>
2026-01-15 14:08:25 +0600

    try

--- 5th assignment/src/main.jsx
+++ 5th assignment/src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import { RouterProvider } from "react-router";
 import router from "./routers/router";
-import DataProvider from "./providers/dataProvider";
+import DataProvider from "./providers/DataProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
